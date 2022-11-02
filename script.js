28317cce7776f4a5d5268e7fe3331a8a4ba57d1e
ZZ83 <13670661+ZZ83@users.noreply.github.com>
2022-11-02 16:28:52 -0700

    Fix nav scrolling issue and hero text spacing

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const mainNav   = document.querySelector('.primary-navigation');
 const navToggle = document.querySelector('.mobile-menu-icons');
+const body = document.querySelector('body');
 
 navToggle.addEventListener('click', () => {
     const vis = mainNav.getAttribute("data-visible");
     if (vis === "false") {
         mainNav  .setAttribute("data-visible",  true);
         navToggle.setAttribute("aria-expanded", true);
+        body.style.overflowY = "hidden";
+
     } else if (vis === "true") {
         mainNav  .setAttribute("data-visible",  false);
         navToggle.setAttribute("aria-expanded", false);
+        body.style.overflowY = "scroll"
     }
 })
+
